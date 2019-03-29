38c47479cd2060995e8046c586d75338ec68bcc6
xiaohui <1002714042@qq.com>
2019-03-29 17:05:06 +0800

    Update reverse_str.c

--- reverse_str.c
+++ reverse_str.c
@@ -1,19 +1,22 @@
-include <stdio.h>
+#include <stdio.h>
 
 
 void main()
 {
 	printf("please enter string\n");
 	reverse();
+	printf("\n");
+	return ;
 }
 
 void reverse()
 {
 	char c;
 	
-	scanf("%c",c);
+	scanf("%c",&c);
 	if(c != '\n'){
 		reverse();
 		printf("%c",c);
 	}
+	return ;
 }
